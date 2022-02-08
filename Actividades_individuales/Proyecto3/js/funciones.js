0ead54b8f890a912a762f75c3fbc01b1bdda7d14
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-02-08 13:22:36 +0000

    Actividades

--- Actividades_individuales/Proyecto3/js/funciones.js
+++ Actividades_individuales/Proyecto3/js/funciones.js
@@ -41,13 +41,8 @@ function loteria() {
   return numeroGanador;
 }
 
-
-
-
-
-
-//problema si se da la palabra correcta en la ultima vuelta del bucle no hace la ultima verificacion
 function adivinaPalabra() {
+  //problema si se da la palabra correcta en la ultima vuelta del bucle no hace la ultima verificacion
   var palabraOculta = prompt("Ingresa la palabra a advinidar");
   var palabraIngresada = prompt("Adivina la palabra");
   intentos = 2;
@@ -59,35 +54,50 @@ function adivinaPalabra() {
     intentos--;
   } while (intentos > 0);
 
-  
   if (validacion != true) {
     alert("Mas suerte a la proxima! La palabra era:" + palabraOculta);
   }
 }
 
-
-
-
-
-
 function adivinaPalabra2() {
-    var palabraOculta = prompt("Ingresa la palabra a advinidar");
-    var palabraIngresada = prompt("Adivina la palabra");
-    var validacion;
-    var intentos = 2;
-  
-    do {
-      validacion = palabraIngresada == palabraOculta;
-      validacion
-        ? alert("Correcto!!")
-        : prompt("Incorrecto, te quedan " + intentos + " intentos");
-      intentos--;
-    } while (intentos > 0);
-  
+  var palabraOculta = prompt("Ingresa la palabra a advinidar");
+  var palabraIngresada = prompt("Adivina la palabra");
+  var validacion;
+  var intentos = 2;
+
+  do {
     validacion = palabraIngresada == palabraOculta;
-    if (validacion != true) {
-      alert("Mas suerte a la proxima! La palabra era:" + palabraOculta);
-    }else{
-        alert("Correcto!!")
-    }
+    validacion
+      ? alert("Correcto!!")
+      : prompt("Incorrecto, te quedan " + intentos + " intentos");
+    intentos--;
+  } while (intentos > 0);
+
+  validacion = palabraIngresada == palabraOculta;
+  if (validacion != true) {
+    alert("Mas suerte a la proxima! La palabra era:" + palabraOculta);
+  } else {
+    alert("Correcto!!");
   }
+}
+
+function encuentraCaracter() {
+  var palabra = prompt("Ingrese una palabra");
+  var caracter = prompt("Ingrese el caracter que desea contar de la palabra dada");
+  var cortador = 0;
+  var index = 0;
+  var coincidencias = 0;
+
+  while (index != palabra.length + 1) {
+    let caracterCortado = palabra.slice(cortador, cortador + 1);
+
+    if (caracter == caracterCortado) {
+      coincidencias++;
+    }
+    
+    index++;
+    cortador++;
+  }
+
+  alert("Se encontraron: " +coincidencias +" coincidencias del caracter en la palabra");
+}
