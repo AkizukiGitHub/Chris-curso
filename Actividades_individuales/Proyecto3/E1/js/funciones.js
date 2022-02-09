05170eabb4fb8315be45a57a7f3b374010427e4a
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-02-09 10:20:58 +0000

    Actividades

---
+++ Actividades_individuales/Proyecto3/E1/js/funciones.js
@@ -0,0 +1,57 @@
+// Dados dos números a y b hallar el mayor. 
+
+function CoomparaPar() {
+    
+}
+
+// Dados 3 números a, b, c  determinar el mayor de los tres
+
+function CoomparaTrio() {
+    
+}
+
+// Determinar si un número dado es negativo, cero o positivo
+
+function DeterminaValor() {
+    
+}
+
+// Pedir el nombre y la edad de un usuario. Determinar si puede votar
+
+function DeterminaMayorEdad() {
+    
+}
+
+// Imprimir los números del 1 a 200
+
+function ImprimeNumeros() {
+    
+}
+
+/* Dada la edad de un pasajero, saber si es bebé, niño o adulto para pagar un boleto aéreo, teniendo en cuenta que:
+El billete cuesta 200⋲
+Los bebés de menos de 2 años pagan el 50%
+Los niños de menos de 12 años pagan el 75%
+Los mayores de 12 años pagan el 100%*/
+
+function DeterminaValorVuelo() {
+    
+}
+
+// Obtener el factorial de un número: n!, teniendo en cuenta que el factorial se calcula n!=n*(n-1)*(n-2)….*1 y el factorial de 0! es 1
+
+function ObtenerFactorial() {
+    
+}
+
+// Dado un número del 1 al 12, determinar el mes correspondiente y mostrar el nombre en letras del mismo
+
+function DeterminarMes() {
+    
+}
+
+// Obtener la nota definitiva de 1 alumno, sabiendo que cada alumno tiene tres evaluaciones de proceso (E1,E2,E3) que representan el 30% de la nota final y que además el otro 70% está compuesto por una prueba objetiva del 20% y una prueba práctica del 80%.
+
+function ObtenerNota() {
+    
+}
