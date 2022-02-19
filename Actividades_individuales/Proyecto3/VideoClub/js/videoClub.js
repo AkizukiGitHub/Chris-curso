733c4a34df04e469e360c165bcb7871566e4df3b
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-02-19 17:25:15 +0000

    Actividades

--- Actividades_individuales/Proyecto3/VideoClub/js/videoClub.js
+++ Actividades_individuales/Proyecto3/VideoClub/js/videoClub.js
@@ -1,16 +1,27 @@
-var index=0;
-index++;
-var cliente = "cliente0"+index;
-
-class prueba {
-    constructor(x){
-        this.x = x;
+class Socio{
+    CreaSocio(nombre,apellidos,direccion,telefono,directorPref,generoPref){
+        this.nombres = nombre;
+        this.apellidos = apellidos;
+        this.direccion = direccion;
+        this.telefono = telefono;
+        this.directorPref = directorPref;
+        this.generoPref = generoPref;
     }
 }
 
+class Peliculas{
+    CreaPeliculas(titulo,genero,director,actores){
+        this.titulo = titulo;
+        this.genero = [genero];
+        this.director = director;
+        this.actores = [actores];
+    }
+}
 
-var cliente = new prueba("Holas");
-index++;
-console.log(cliente01.x)
-var cliente = new prueba("Adios");
-console.log(cliente.x)
+class Cintas extends Peliculas{
+    CreaCintas(titulo,genero,director,actores,numCinta,disponible){
+        super(titulo,genero,director,actores);
+        this.numCinta = numCinta;
+        this.disponible = disponible;
+    }
+}
