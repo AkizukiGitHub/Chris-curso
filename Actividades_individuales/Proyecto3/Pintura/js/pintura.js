db9d4fe7af936873b4418c6d80466fb454d395d0
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-02-24 13:01:16 +0000

    Actividades

---
+++ Actividades_individuales/Proyecto3/Pintura/js/pintura.js
@@ -0,0 +1,21 @@
+class cliente{
+    constructor(nom,ape,id,dir,tlf,mp,imp){
+        this.nombres = nom;
+        this.apellidos = ape;
+        this.DNI = id;
+        this.Direccion = dir;
+        this.Movil = tlf;
+        this.MetodoPago = mp;
+        this.DatosMetodoPago = imp;
+    }
+}
+class pintura{
+    constructor(cod,desc,pre){
+        this.Codigo =cod ;
+        this.Descripccion = desc;
+        this.Precio = pre;
+    }
+}
+function calcularIGIC(x) {
+  return x * 0,07;
+}
