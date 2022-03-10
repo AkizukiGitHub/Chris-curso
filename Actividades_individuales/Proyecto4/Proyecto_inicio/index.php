07c06346f88555ab5be14cb692e42d7207afc1bb
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-03-10 13:29:21 +0000

    Actividades

---
+++ Actividades_individuales/Proyecto4/Proyecto_inicio/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $nombre = "Chris";
+        echo "Mi primer echo viene de: $nombre";
+        echo "<br>";
+        print "Segundo mensaje de la pagina desde el php" 
+    ?>
+</body>
+</html>
