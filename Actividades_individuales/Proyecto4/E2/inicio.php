e3afea47b1661826c069d03056f4ab7d95354b4c
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-03-23 13:01:10 +0000

    actividades

---
+++ Actividades_individuales/Proyecto4/E2/inicio.php
@@ -0,0 +1,71 @@
+<?php
+session_start();
+class Cliente
+{
+    protected $nombre;
+    protected $apellidos;
+    protected $DNI;
+    
+    function __construct($nombre, $apellidos, $DNI) {
+        $this->nombre = $nombre;
+        $this->apellidos = $apellidos;
+        $this->DNI = $DNI;
+    }
+	function getNombre() {
+        return $this->nombre;
+	}
+	function setNombre($nombre){
+        $this->nombre = $nombre;
+		return $this;
+	}
+	function getApellidos() {
+        return $this->apellidos;
+	}
+	function setApellidos($apellidos){
+        $this->apellidos = $apellidos;
+		return $this;
+	}
+	function getDNI() {
+        return $this->DNI;
+	}
+	function setDNI($DNI){
+        $this->DNI = $DNI;
+		return $this;
+	}
+    
+}
+class Cuenta extends Cliente{
+    protected $IBAN;
+
+    function __construct($IBAN) {
+        $this->IBAN = $IBAN;
+    }
+	function getIBAN() {
+		return $this->IBAN;
+	}
+	function setIBAN($IBAN){
+		$this->IBAN = $IBAN;
+		return $this;
+	}
+}
+
+
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>E2</title>
+</head>
+<body>
+    <form action="" method="get" name="form1">
+        <input type="text" name="nombre" placeholder="nombre">
+        <input type="text" name="apellidos" placeholder="apellidos">
+        <input type="text" name="dni" placeholder="dni">
+        <input type="text" name="IBAN" placeholder="IBAN">
+        <input type="submit" name="btn1">
+    </form>
+</body>
+</html>
---
+++ Actividades_individuales/Proyecto4/ejemploPO03.php
@@ -0,0 +1,105 @@
+<?php
+session_start();
+class Persona {
+    public $nombre; 
+    public $apellidos;
+    public $dni;  
+    public $movil; 
+    public $email; 
+    public $edad;
+    public $vivo;
+    
+    function __construct($nombre,$apellidos,$dni,$movil,$email,$edad,$vivo) { 
+        $this->nombre=$nombre;
+        $this->apellidos=$apellidos;
+        $this->dni=$dni;
+        $this->movil=$movil;
+        $this->email=$email;
+        $this->edad=$edad;
+        $this->vivo=$vivo; 
+
+     }
+
+     function get_nombre() { 
+        return $this->nombre;
+    }
+
+    function set_vivo() { 
+        $this->vivo=false;
+        return $this->vivo;
+    }
+
+ }
+
+//  class Coche extends Persona {
+//      #código
+//      public $marca;
+//      function get_marca() { 
+//         return $this->marca;
+//     }
+//     function set_marca($x) { 
+//         $this->marca=$x;
+//         return $this->marca;
+//     }
+//  } 
+
+//  $objeto1=new Coche($_GET["nombre"],$_GET["apellidos"],$_GET["dni"],$_GET["movil"],$_GET["email"],$_GET["edad"],$_GET["vivo"]);
+
+//     echo $objeto1->set_marca("Volvo");
+
+
+
+// aqui viene lo bueno
+//Opción 1
+//  $nombre=$_GET["nombre"];
+// $apellidos=$_GET["apellidos"];
+// $dni=$_GET["dni"];
+// $movil=$_GET["movil"];
+// $email=$_GET["email"];
+// $edad=$_GET["edad"];
+// $vivo=$_GET["vivo"];
+
+// $objeto1 = new Persona($nombre,$apellidos,$dni, $movil,$email,$edad,$vivo);
+
+//Opción 2
+if(isset($_GET["btn1"])){
+if(empty($_SESSION["objetos"])){
+    $_SESSION["objetos"]=[];
+    array_push($_SESSION["objetos"], new Persona($_GET["nombre"],$_GET["apellidos"],$_GET["dni"],$_GET["movil"],$_GET["email"],$_GET["edad"],$_GET["vivo"])); 
+}else{
+     
+        array_push($_SESSION["objetos"], new Persona($_GET["nombre"],$_GET["apellidos"],$_GET["dni"],$_GET["movil"],$_GET["email"],$_GET["edad"],$_GET["vivo"]));        
+           
+     }
+     echo "<table border=1>";
+    foreach($_SESSION["objetos"] as $cajita){
+        echo "<tr>";
+        echo "<td>".$cajita->get_nombre(). "</td>";
+        echo "<td>".$cajita->apellidos . "</td> ";
+        echo "</tr>";
+    } // cierra el foreach
+ echo "</table>";
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Probado cosas</title>
+</head>
+<body>
+    <form action="" method="get" name="form1">
+        <input type="text" name="nombre" placeholder="nombre">
+        <input type="text" name="apellidos">
+        <input type="text" name="dni" placeholder="dni">
+        <input type="text" name="movil" placeholder="movil">
+        <input type="email" name="email">
+        <input type="number" name="edad" placeholder="edad">
+        vivo:<input type="radio" value="true" name="vivo">
+        muerto<input type="radio" value="false" name="vivo" checked>
+        <input type="submit" name="btn1">
+    </form>
+</body>
+</html>
