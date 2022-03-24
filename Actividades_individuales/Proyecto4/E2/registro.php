2f0ea7c629b7ab9a3331f5f39ca855b455e73090
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-03-24 10:17:01 +0000

    Actividades

--- Actividades_individuales/Proyecto4/E2/registro.php
+++ Actividades_individuales/Proyecto4/E2/registro.php
@@ -1,11 +1,12 @@
 <?php
 session_start();
+// inicio la sesion que se usara como base de datos
 class Cliente
 {
     protected $nombre;
     protected $apellidos;
     protected $DNI;
-
+    // creo la clase Cliente y le doy 3 atributos
 	function getNombre() {
         return $this->nombre;
 	}
@@ -27,12 +28,12 @@ class Cliente
         $this->DNI = $DNI;
 		return $this;
 	}
-    
+    // creo todos los getter y los setters
 }
 class Cuenta extends Cliente
 {
     protected $IBAN;
-
+    // creo la clase cuenta que hereda de la clase cliente y le agrego el IBAN
 	function getIBAN() {
 		return $this->IBAN;
 	}
@@ -40,15 +41,21 @@ class Cuenta extends Cliente
 		$this->IBAN = $IBAN;
 		return $this;
 	}
+    // getters y setters de la clase cuenta
 }
 
 $listaCuentas = "<table border=1>";
+// lista cuentas es una variable que concatena toda las etiquetas y elementos de la tabla
 $cuentaEncontrada = 0;
+// cuenta encontrada se usa para verificar que se encontro una cuenta con el DNI indicado incrementando su valor
 
 if (isset($_GET["btn1"])) {
+    // este if esta relacionado con el primer formulario el del registro
     if(empty($_SESSION["objetos"])){
+        // doble condicional que ocurre si se activo el boton y no hay nada en la tapa objetos de la session
+        // al entrar en el if creo el array dentro de objetos y meto los elementos del formulario en la sesion
         $_SESSION["objetos"]=[];
-
+        
         array_push($_SESSION["objetos"],
         (new Cuenta())
             ->setNombre($_GET["nombre"])
@@ -57,6 +64,7 @@ if (isset($_GET["btn1"])) {
             ->setIBAN($_GET["IBAN"]));
     }
     else {
+        // al llegar al else es que se activo el boton y objetos no esta vacio se hace un push al array para meter nuevos clientes
         array_push($_SESSION["objetos"],
         (new Cuenta())
             ->setNombre($_GET["nombre"])
@@ -66,21 +74,27 @@ if (isset($_GET["btn1"])) {
 }
 }
 if(isset($_GET["btn2"])){
+    // este if esta relacionado con el segundo formulario el de la busqueda
     if(empty($_SESSION["objetos"])) {
+        // si se activa el boton y esta vacio ese envia este mensaje
         echo "No hay cientes registrados".PHP_EOL."\n";
     }else{
+        // si hay elementos dentro de objetos empieza la busqueda
         foreach ($_SESSION["objetos"] as $index=>$cuenta) {
             if(($cuenta->getDNI()==$_GET["DNI"])){
+                // verifica que el DNI ingresado por el formulario sea igual que el DNI del objeto sacado por el foreach
+                // si lo encuentra añade una nueva fila y la rellena con todos los elementos
                 $listaCuentas .= "<tr>";
                 $listaCuentas .= "<td>".$_SESSION["objetos"][$index]->getNombre(). "</td>";
                 $listaCuentas .= "<td>".$_SESSION["objetos"][$index]->getApellidos(). "</td>";
                 $listaCuentas .= "<td>".$_SESSION["objetos"][$index]->getDNI(). "</td>";
                 $listaCuentas .= "<td>".$_SESSION["objetos"][$index]->getIBAN(). "</td>";
                 $listaCuentas .= "</tr>";
-
+                // incremento cuenta encontrada que es el verificador
                 $cuentaEncontrada++;
             }
         }
+        // este if decide si se imprime la lista o no segun el verificador cuentaEncontrada
         if ($cuentaEncontrada>0) {
             echo $listaCuentas;
         }else {
