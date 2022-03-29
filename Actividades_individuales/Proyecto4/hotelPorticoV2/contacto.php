6a660d24258854a1a2de2eca6367b5e168adeb90
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-03-29 11:57:55 +0100

    actividades

--- Actividades_individuales/Proyecto4/hotelPorticoV2/contacto.php
+++ Actividades_individuales/Proyecto4/hotelPorticoV2/contacto.php
@@ -1,43 +1,6 @@
-<!DOCTYPE html>
-<html lang="es">
-
-<head>
-  <meta charset="UTF-8" />
-  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-  <title>Hotel Pórtico</title>
-  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
-  <link rel="stylesheet" href="css/hotel.css" />
-</head>
-
-<body>
-  <!-- inicio barra de navegacion, solamente funciona el boton de contacto e inicio -->
-  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="index.html">Bienvenido al Hotel Pórtico</a>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="#">Habitaciones</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Restaurantes</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Piscinas</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="#">Recreacion</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" href="contacto.html" id="B_contacto"><strong>Contacto</strong></a>
-          </li>
-        </ul>
-      </div>
-    </div>
-  </nav>
-  <!-- cierre barra de navegacion -->
+<?php
+include("includes/nav.php");
+?>
 
   <h3 id="tituloCont">Contactanos</h3>
   <div class="Section1">
@@ -75,11 +38,8 @@
         <input type='radio' name='gender' value='Femenino' /><label>Femenino</label><br />
         <input type='radio' name='gender' value='Sin especificar' /><label>Prefiero no decirlo</label><br />
       </fieldset>
-      <input type='submit' value='Enviar' />"
+      <input type='submit' value='Enviar' />
+    </section>";
+
+    include("includes/cierre.php");
 ?>
-
-      </section>
-
-</body>
-
-</html>
---
+++ Actividades_individuales/Proyecto4/hotelPorticoV2/includes/nav.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+  <meta charset="UTF-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Hotel Pórtico</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous" />
+  <link rel="stylesheet" href="css/hotel.css" />
+</head>
+
+<body>
+  <!-- inicio barra de navegacion, solamente funciona el boton de contacto e inicio -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.html">Bienvenido al Hotel Pórtico</a>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">Habitaciones</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Restaurantes</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Piscinas</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#">Recreacion</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contacto.html" id="B_contacto"><strong>Contacto</strong></a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
