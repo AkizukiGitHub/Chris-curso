919c28b1e77213a6eb5fe138de7f64bef3f038f4
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-03-30 12:14:19 +0100

    avtividades

--- Actividades_individuales/Proyecto4/hotelPorticoV2/reserva.php
+++ Actividades_individuales/Proyecto4/hotelPorticoV2/reserva.php
@@ -2,30 +2,30 @@
 include("includes/nav.php");
 
 ?>
-
 <?php
-  getdate();
+getdate();
   $hoy = getdate();
-  $min = ($hoy['year']-18)."-0".$hoy['mon']."-".$hoy['mday'];
+  $min = $hoy['year']."-0".$hoy['mon']."-".$hoy['mday'];
+  $max = ($hoy['year']+10)."-12-31";
 
-echo
-"<form>
-    <section style='margin-left: 20%; margin-right: 30%;'>
-        <h3>Paso 1</h3>
-        <legend class='border border-dark'>Datos personales
-            <fieldset style='width: 100%'>
-                <label>Email:</label><input type='email' name='email' /><br /><br />
-                <label>Nombre : </label><input type='text' name='fname' /><span style='color: red;'>*</span><br /><br />
-                <label>Apellido : </label><input type='text' name='lname' /><span style='color: red;'>*</span><br /><br />
-                <label>Sexo:</label><br/>
-                <input type='radio' name='gender' value='Masculino' /><label>Masculino</label><br />
-                <input type='radio' name='gender' value='Femenino' /><label>Femenino</label><br />
-                <input type='radio' name='gender' value='Sin especificar' /><label>Prefiero no decirlo</label><br />
-                <label>Fecha de nacimiento:</label>
-                <input type='date' name='fnac' max='$min'/><br/>
-            </legend>
-        </fieldset>
-        <input type='submit' value='Enviar' />
-    </section>
-</form>";
+  print_r($min);
+  echo "<br>";
+  print_r($max);
+
+ echo 
+ "<form action='' method='post'>
+    <section style='margin-left: 20%; margin-right: 20%;'>
+      <fieldset style='width: 100%'>
+        <label>Email:</label><input type='email' name='email' /><br /><br />
+        <label>Nombre : </label><input type='text' name='fname' /><span style='color: red;'>*</span><br /><br />
+        <label>Apellido : </label><input type='text' name='lname' /><span style='color: red;'>*</span><br /><br />
+        <label> Fecha de nacimiento:</label>
+        <input type='date' name='fnac' min='$min' max='$max'/><br/>
+        <br>
+        <label>Sexo:</label><br/>
+        <input type='radio' name='gender' value='Masculino' /><label>Masculino</label><br />
+        <input type='radio' name='gender' value='Femenino' /><label>Femenino</label><br />
+        <input type='radio' name='gender' value='Sin especificar' /><label>Prefiero no decirlo</label><br />
+      </fieldset>
+      <input type='submit' value='Enviar' />"
 ?>
