c5db9d460837efa26ea0b865e78225ad34bf3639
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-06-02 13:50:35 +0100

    act

---
+++ Actividades_individuales/Libreria/libreria.sql
@@ -0,0 +1,264 @@
+-- phpMyAdmin SQL Dump
+-- version 5.1.1
+-- https://www.phpmyadmin.net/
+--
+-- Servidor: 127.0.0.1
+-- Tiempo de generación: 02-06-2022 a las 14:50:04
+-- Versión del servidor: 10.4.22-MariaDB
+-- Versión de PHP: 7.4.27
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de datos: `libreria`
+--
+CREATE DATABASE IF NOT EXISTS `libreria` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_spanish_ci;
+USE `libreria`;
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detallesfacturas`
+--
+
+CREATE TABLE `detallesfacturas` (
+  `id` int(11) NOT NULL,
+  `idFactura` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL,
+  `cantidad` int(10) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `detallesfacturas`
+--
+
+INSERT INTO `detallesfacturas` (`id`, `idFactura`, `idProducto`, `cantidad`) VALUES
+(1, 1, 1, 1);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `detalleusuario`
+--
+
+CREATE TABLE `detalleusuario` (
+  `id` int(11) NOT NULL,
+  `telefono` varchar(9) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `direccion` varchar(100) COLLATE utf8mb4_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `detalleusuario`
+--
+
+INSERT INTO `detalleusuario` (`id`, `telefono`, `direccion`) VALUES
+(2, NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `factura`
+--
+
+CREATE TABLE `factura` (
+  `id` int(11) NOT NULL,
+  `idCliente` int(11) NOT NULL,
+  `fecha` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `factura`
+--
+
+INSERT INTO `factura` (`id`, `idCliente`, `fecha`) VALUES
+(1, 2, '2022-06-02 14:23:26');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `inventario`
+--
+
+CREATE TABLE `inventario` (
+  `id` int(11) NOT NULL,
+  `idProducto` int(11) NOT NULL,
+  `cantidad` int(11) NOT NULL,
+  `precioUnitario` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `inventario`
+--
+
+INSERT INTO `inventario` (`id`, `idProducto`, `cantidad`, `precioUnitario`) VALUES
+(1, 1, 10, 19);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `productos`
+--
+
+CREATE TABLE `productos` (
+  `id` int(11) NOT NULL,
+  `titulo` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `idioma` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `numPag` varchar(8) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `ISBN-10` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `ISBN-13` varchar(20) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `precio` decimal(10,2) DEFAULT NULL,
+  `tituloLargo` varchar(100) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `autor` varchar(50) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `genero` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `tema` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL,
+  `tipoEncuadernacion` varchar(50) COLLATE utf8mb4_spanish_ci DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `productos`
+--
+
+INSERT INTO `productos` (`id`, `titulo`, `idioma`, `numPag`, `ISBN-10`, `ISBN-13`, `precio`, `tituloLargo`, `autor`, `genero`, `tema`, `tipoEncuadernacion`) VALUES
+(1, 'El imperio final', 'Español', '688', '8417347291', '978-8417347291', NULL, 'El imperio final (Nacidos de la bruma [Mistborn] 1)', 'Brandon Sanderson', 'Fantasia', NULL, NULL);
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `usuariologin`
+--
+
+CREATE TABLE `usuariologin` (
+  `id` int(11) NOT NULL,
+  `correo` varchar(254) COLLATE utf8mb4_spanish_ci NOT NULL,
+  `clave` varchar(254) COLLATE utf8mb4_spanish_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
+
+--
+-- Volcado de datos para la tabla `usuariologin`
+--
+
+INSERT INTO `usuariologin` (`id`, `correo`, `clave`) VALUES
+(2, 'chris@correo.se', '123456789');
+
+--
+-- Índices para tablas volcadas
+--
+
+--
+-- Indices de la tabla `detallesfacturas`
+--
+ALTER TABLE `detallesfacturas`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idFactura` (`idFactura`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `detalleusuario`
+--
+ALTER TABLE `detalleusuario`
+  ADD UNIQUE KEY `id_2` (`id`),
+  ADD KEY `id` (`id`);
+
+--
+-- Indices de la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idCliente` (`idCliente`);
+
+--
+-- Indices de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idProducto` (`idProducto`);
+
+--
+-- Indices de la tabla `productos`
+--
+ALTER TABLE `productos`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indices de la tabla `usuariologin`
+--
+ALTER TABLE `usuariologin`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `correo_2` (`correo`),
+  ADD KEY `correo` (`correo`);
+
+--
+-- AUTO_INCREMENT de las tablas volcadas
+--
+
+--
+-- AUTO_INCREMENT de la tabla `detallesfacturas`
+--
+ALTER TABLE `detallesfacturas`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `factura`
+--
+ALTER TABLE `factura`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `productos`
+--
+ALTER TABLE `productos`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT de la tabla `usuariologin`
+--
+ALTER TABLE `usuariologin`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- Restricciones para tablas volcadas
+--
+
+--
+-- Filtros para la tabla `detallesfacturas`
+--
+ALTER TABLE `detallesfacturas`
+  ADD CONSTRAINT `detallesfacturas_ibfk_1` FOREIGN KEY (`idFactura`) REFERENCES `factura` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `detallesfacturas_ibfk_2` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `detalleusuario`
+--
+ALTER TABLE `detalleusuario`
+  ADD CONSTRAINT `detalleusuario_ibfk_1` FOREIGN KEY (`id`) REFERENCES `usuariologin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `factura`
+--
+ALTER TABLE `factura`
+  ADD CONSTRAINT `factura_ibfk_1` FOREIGN KEY (`idCliente`) REFERENCES `usuariologin` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Filtros para la tabla `inventario`
+--
+ALTER TABLE `inventario`
+  ADD CONSTRAINT `inventario_ibfk_1` FOREIGN KEY (`idProducto`) REFERENCES `productos` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
