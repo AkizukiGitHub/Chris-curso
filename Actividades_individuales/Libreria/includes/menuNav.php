18fdadcc8daf6293066cad5e9fb60eceacd7d997
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-06-09 13:36:34 +0100

    act

--- Actividades_individuales/Libreria/includes/menuNav.php
+++ Actividades_individuales/Libreria/includes/menuNav.php
@@ -3,18 +3,21 @@
         <div class="col-3 p-2">
             <a class="navbar-brand d-flex justify-content-center" href="#">LOGO</a>
         </div>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID" aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarID"
+            aria-controls="navbarID" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <div class="collapse navbar-collapse col-9" id="navbarID">
             <form class="form col-7">
                 <div class="input-group">
-                    <input class="form-control" type="search" placeholder="Buscar por autor, título, género, ISBN" aria-label="Search">
+                    <input class="form-control" type="search" placeholder="Buscar por autor, título, género, ISBN"
+                        aria-label="Search">
                     <button class="btn-search btn !important" type="submit" style="color:#000000;">Search</button>
                 </div>
             </form>
             <div class="col-2 align-self-end">
-                <button class="btn btn-sample mr-2 mt-2 mb-2" type="button" data-bs-toggle="offcanvas" data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
+                <button class="btn btn-sample mr-2 mt-2 mb-2" type="button" data-bs-toggle="offcanvas"
+                    data-bs-target="#offcanvasRight" aria-controls="offcanvasRight">
                     <span class="material-symbols-outlined">account_circle</span><br><span>Identificate</span></button>
             </div>
         </div>
@@ -31,25 +34,35 @@
                 <h3>Iniciar sesion</h3>
                 <div class="form-group">
                     <label for="email"><strong>Correo:</strong></label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Escribir correo" required>
-                    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su informacion con otros</small>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                        placeholder="Escribir correo" required>
+                    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su informacion con
+                        otros</small>
                 </div>
                 <div class="form-group">
                     <label for="password"><strong>Contraseña:</strong></label>
                     <input type="password" class="form-control" id="password" placeholder="Contraseña" required>
+                    <small class="form-text text-muted">La contraseña debe tener entre 8 y 16 caracteres
+                        <br>al menos una letra mayuscula y una misculas
+                        <br>al menos un numero
+                        <br>al menos un caracter especial
+                        <br>no puede contener espacios
+                    </small>
                 </div>
                 <div class="form-group form-check">
                     <input type="checkbox" class="form-check-input" id="exampleCheck1">
                     <label class="form-check-label" for="exampleCheck1">Recuerdame</label>
                 </div>
-                <button type="submit" class="btn-search btn w-100" style="color:#000000;">Iniciar sesion</button>    
+                <button type="submit" class="btn-search btn w-100" style="color:#000000;">Iniciar sesion</button>
             </form>
             <form action="" style="visibility: hidden; max-height: 0%;" id="signInform">
                 <h3>Registrarse</h3>
                 <div class="form-group">
                     <label for="email"><strong>Correo:</strong></label>
-                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp" placeholder="Escribir correo" required>
-                    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su informacion con otros</small>
+                    <input type="email" class="form-control" id="email" aria-describedby="emailHelp"
+                        placeholder="Escribir correo" required>
+                    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su informacion con
+                        otros</small>
                 </div>
                 <div class="form-group">
                     <label for="password"><strong>Contraseña:</strong></label>
@@ -57,19 +70,33 @@
                 </div>
                 <div class="form-group">
                     <label for="password"><strong>Confirmar contraseña:</strong></label>
-                    <input type="password" class="form-control" id="confirm_password" placeholder="Contraseña" required>
+                    <input onchange="" type="password" class="form-control" id="confirm_password"
+                        placeholder="Repetir contraseña" required>
                 </div>
+                <div id="error_message" class="alert alert-danger" role="alert" style="display: none">
+                </div>
+                <small class="form-text text-muted">
+                    <br>La contraseña debe tener entre 8 y 16 caracteres
+                    <br>Deben tener el mismo tamaño
+                    <br>Al menos una letra mayuscula y una minuscula
+                    <br>Al menos un numero
+                    <br>Al menos un caracter especial
+                    <br>No puede contener espacios
+                </small>
+
                 <br>
                 <button type="submit" class="btn-search btn w-100" style="color:#000000;">Registrarse</button>
             </form>
             <hr class="m-2">
             <div class="text-center" id="signInButton" style="visibility: visible;">
                 <h3>¿Eres nuevo?</h3>
-                <button onclick="mostrar_ocultar('logInForm_signInform_signInButton_logInButton')" class="btn-search btn fluid" style="color:#000000;">Registrarse</button>
+                <button onclick="mostrar_ocultar('logInForm_signInform_signInButton_logInButton')"
+                    class="btn-search btn fluid" style="color:#000000;">Registrarse</button>
             </div>
             <div class="text-center" id="logInButton" style="visibility: hidden;">
                 <h3>¿Ya tienes cuenta?</h3>
-                <button onclick="mostrar_ocultar('logInForm_signInform_signInButton_logInButton')" class="btn-search btn fluid" style="color:#000000;">Iniciar sesion</button>
+                <button onclick="mostrar_ocultar('logInForm_signInform_signInButton_logInButton')"
+                    class="btn-search btn fluid" style="color:#000000;">Iniciar sesion</button>
             </div>
         </div>
     </div>
@@ -80,4 +107,3 @@
         border-color: #FF0000;
         box-shadow: inset 0 1px 1px rgba(00, 0, 0, 0.075),0 0 8px rgba(255, 0, 0, 0.6);
     } -->
-    
--- Actividades_individuales/Libreria/js/funciones.js
+++ Actividades_individuales/Libreria/js/funciones.js
@@ -1,19 +1,41 @@
-function mostrar_ocultar(string){
-    var elemento = string;
-    var elementos = elemento.split("_");
-    var longitud = elementos.length;
-    
+function mostrar_ocultar(string) {
+  var elemento = string;
+  var elementos = elemento.split("_");
+  var longitud = elementos.length;
 
-    for(i=0;i<longitud;i++){
-        var elemento = document.getElementById(elementos[i]);
-        if(elemento.style.visibility == "hidden"){
-            elemento.style.visibility = "visible";
-            elemento.style.maxHeight = "100%";
-        }
-        else if(elemento.style.visibility == "visible"){
-            elemento.style.visibility = "hidden";
-            elemento.style.maxHeight = "0px";
-        }
+  for (i = 0; i < longitud; i++) {
+    var elemento = document.getElementById(elementos[i]);
+    if (elemento.style.visibility == "hidden") {
+      elemento.style.visibility = "visible";
+      elemento.style.maxHeight = "100%";
+    } else if (elemento.style.visibility == "visible") {
+      elemento.style.visibility = "hidden";
+      elemento.style.maxHeight = "0px";
     }
+  }
 }
 
+function validar_password(password, confirm_password) {
+  let igual_o_distinta = compare_password(password, confirm_password);
+  if (igual_o_distinta) {
+    validar_patron(password);
+  } else {
+    ("Las contraseñas no coinciden");
+  }
+}
+
+
+
+function compare_password(password, confirm_password) {
+  if (password === confirm_password) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function validar_patron(password) {
+  var re =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])([A-Za-z\d$@$!%*?&]|[^ ]){8,16}$/;
+  return re.test(password);
+}
