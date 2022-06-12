1b284cac1db0d21d9edd0af7d3f036f6fe6c75ff
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-06-12 23:09:56 +0100

    act

--- Actividades_individuales/Libreria/inicio.php
+++ Actividades_individuales/Libreria/inicio.php
@@ -2,8 +2,137 @@
     include("includes/head.php");
     include("includes/menuNav.php");
 ?>
+<div class="container">
 
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Libreria</h1>
+        </div>
+    </div>
 
+    <div class="flex-container">
+        <div class="col-md-12">
+            <div class="card-deck flex-container">
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 1</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro1">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 2</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro2">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 3</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro3">Ver</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex-container row">
+        <div class="col-md-12">
+            <div class="card-deck flex-container">
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 4</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro4">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 5</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro5">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 6</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro6">Ver</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex-container row">
+        <div class="col-md-12">
+            <div class="card-deck flex-container">
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 7</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro7">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 8</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro8">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 9</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro9">Ver</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="flex-container row">
+        <div class="col-md-12">
+            <div class="card-deck flex-container">
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 10</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro10">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 11</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro11">Ver</a>
+                    </div>
+                </div>
+                <div class="card col-3">
+                    <img class="card-img-top" src="https://picsum.photos/200/300" alt="Card image cap">
+                    <div class="card-body">
+                        <h5 class="card-title">Libro 12</h5>
+                        <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                        <a class="btn add-to-cart libro12">Ver</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php
     include("includes/footer.php");
--- Actividades_individuales/Libreria/js/funciones.js
+++ Actividades_individuales/Libreria/js/funciones.js
@@ -1,3 +1,110 @@
+console.log("funciones.js cargado 5");
+
+let carts = document.querySelectorAll(".add-to-cart");
+
+let products = [
+  {
+    name: "Producto 1",
+    id: "producto1",
+    price: "$100",
+    cantidad: 0
+  },
+  {
+    name: "Producto 2",
+    id: "producto2",
+    price: "$200",
+    cantidad: 0
+  },
+  {
+    name: "Producto 3",
+    id: "producto3",
+    price: "$300",
+    cantidad: 0
+  },
+  {
+    name: "Producto 4",
+    id: "producto4",
+    price: "$400",
+    cantidad: 0
+  },
+  {
+    name: "Producto 5",
+    id: "producto5",
+    price: "$500",
+    cantidad: 0
+  },
+  {
+    name: "Producto 6",
+    id: "producto6",
+    price: "$600",
+    cantidad: 0
+  },
+  {
+    name: "Producto 7",
+    id: "producto7",
+    price: "$700",
+    cantidad: 0
+  },
+  {
+    name: "Producto 8",
+    id: "producto8",
+    price: "$800",
+    cantidad: 0
+  },
+  {
+    name: "Producto 9",
+    id: "producto9",
+    price: "$900",
+    cantidad: 0
+  },
+  {
+    name: "Producto 10",
+    id: "producto10",
+    price: "$1000",
+    cantidad: 0
+  },
+  {
+    name: "Producto 11",
+    id: "producto11",
+    price: "$1100",
+    cantidad: 0
+  },
+  {
+    name: "Producto 12",
+    id: "producto12",
+    price: "$1200",
+    cantidad: 0
+  }
+];
+
+
+
+for (let i = 0; i < carts.length; i++) {  //recorre todos los elementos de la clase add-to-cart
+  carts[i].addEventListener("click", function () {  //agrega un evento click
+    cartNumbers()
+    }
+  );
+}
+
+function cartNumbers() {
+  let productNumbers = localStorage.getItem("cartNumbers");
+
+  productNumbers = parseInt(productNumbers);
+
+  if (productNumbers) { //si existe el valor en el localStorage
+    localStorage.setItem("cartNumbers", productNumbers + 1);  //aumenta el valor en 1
+    document.querySelector(".cart-number").textContent = productNumbers + 1;  //muestra el valor en el DOM
+  }
+  else {  //si no existe el valor en el localStorage
+    localStorage.setItem("cartNumbers", 1);  //crea el valor en el localStorage
+    document.querySelector(".cart-number").textContent = 1;  //muestra el valor en el DOM
+  } //fin else
+} //fin cartNumbers
+
+
+
+
+
 function mostrar_ocultar(string) {
   var elemento = string;
   var elementos = elemento.split("_");
@@ -23,7 +130,7 @@ function validar_password(password, confirm_password) {
         return true;
         }
     else {
-        document.getElementById("errores").innerHTML = ;
+        // document.getElementById("errores").innerHTML = ;
         }
   } else {
     document.getElementById("errores").innerHTML = '<div class="alert alert-danger d-flex align-items-center" role="alert"><svg class="bi flex-shrink-0 me-2" width="16" height="16" role="img" aria-label="Danger:"><use xlink:href="#exclamation-triangle-fill"/></svg><div><strong>Error!</strong> Las contraseñas no coinciden.</div><div>strong>Error!</strong> Las contraseñas no coinciden.</div></div>';
