bad8b7c79fa464e2ac33e572ce8b54021177e733
AkizukiGitHub <57232755+AkizukiGitHub@users.noreply.github.com>
2022-06-15 10:58:12 +0100

    act

---
+++ Python/parte1/libro.py
@@ -0,0 +1,25 @@
+# message = "hello Python crash course"
+# print(message)
+
+name = "mistborn"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+first_name = "jose"
+last_name = "perez"
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+print(f"Hello, {full_name.title()}!")
+
+print("Languages:\nPython\nC\nJavaScript")
+
+print("Languages:\n\tPython\n\tC\n\tJavaScript")
+
+favorite_language = 'python '
+print(favorite_language.rstrip())
+print(favorite_language)
+
+
+
